150961a5d78c51290e9c57dfe1a4428ebca9f31e
MatthewKompel <matthewkompel@gmail.com>
2023-01-25 20:52:04 -0500

    Added login and add user functions

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const {MongoClient} = require('mongodb');
 const express = require("express");
 const app = express();
+var bcrypt = require('bcryptjs');
 var cors = require('cors');
-
+app.use(express.json())
 app.use(cors({ origin: "http://localhost:8000", optionsSuccessStatus: 200 }));
 
 app.use(function(req, res, next) {
@@ -21,14 +22,58 @@ app.get("/getRiddle", async (req,res) => {
     console.log("RIDDLE",riddle)
     res.send(riddle)
 })
+
+app.post("/addUser", async (req,res) => {
+    //Need to check if the username already exists first, then hold the insert inside a try catch
+    const userPassword = await bcrypt.hash(req.body.password,10)
+    const coll = await connectDB("users")
+    coll.insertOne({
+        username:"test2",
+        password: userPassword
+    })
+    res.send("success")
+})
+app.post("/loginUser", async (req,res) => {
+    
+    const coll = await connectDB("users")
+    console.log(req.body.username)
+    const pipeline = [
+        { 
+            $match: { username: req.body.username } 
+        },
+    ];
+    const aggCursor = await coll.aggregate(pipeline);
+
+    var result
+    for await (const doc of aggCursor) {
+        result = doc
+    }
+    if(!result) {
+        res.send("Invalid Username")
+    } else {
+        console.log(result)
+        const isValid = await bcrypt.compare(req.body.password, result.password);
+        if(isValid) {
+            res.send("successful login")
+        } else {
+            res.send("Invalid Password")
+        }
+    }
+
+})
+
+async function connectDB(collection) {
+    const uri = "mongodb+srv://houser2023:Augustine9009@housercluster.yreyshx.mongodb.net/test"
+    const client = new MongoClient(uri);
+    const res = await client.connect();
+    const db = client.db("Houser");
+    const coll = db.collection(collection);
+    console.log("connected to mongoDB")
+    return coll
+}
 async function connectRiddle() {
     try {
-        const uri = "mongodb+srv://houser2023:Augustine9009@housercluster.yreyshx.mongodb.net/test"
-        const client = new MongoClient(uri);
-        await client.connect();
-        console.log("connected to mongoDB")
-        const db = client.db("Houser");
-        const coll = db.collection("riddles");
+        const coll = await connectDB("riddles")
         const number = Math.floor(Math.random() * 376)
         const pipeline = [
             { 
